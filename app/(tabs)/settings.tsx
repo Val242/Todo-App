d9dd66a20d07b36263912c3432583150c7735fc7
=Val242 <=ebongvalentine70@gmail.com>
2025-08-21 13:59:26 +0100

    Completed the Project

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { createSettingsStyles } from '@/assets/styles/settings.styles';
+import DangerZone from '@/components/DangerZone';
+import Preferences from '@/components/Preferences';
+import ProgressView from '@/components/ProgressView';
+import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Settings = () => {
+const SettingsScreen = () => {
+ 
+
+    const{ colors } = useTheme();
+
+    const settingsStyles = createSettingsStyles(colors);
+
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+
+<LinearGradient colors={colors.gradients.background}  style ={settingsStyles.container}>
+        {/*HEADER*/}
+      <SafeAreaView style = {settingsStyles.safeArea}>
+              <View style={settingsStyles.header}>
+               <View style={settingsStyles.titleContainer}>
+            <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
+              <Ionicons name="settings" size={28} color="#ffffff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+
+        <ScrollView 
+        style={settingsStyles.scrollView}
+          contentContainerStyle={settingsStyles.content}
+          showsVerticalScrollIndicator={false}
+                    >
+
+              <ProgressView />
+              <Preferences />
+              <DangerZone/>
+
+        </ScrollView>
+
+      </SafeAreaView>
+
+</LinearGradient>
+    
   )
 }
 
-export default Settings
+export default SettingsScreen
